a62420fd05b88290e41d9cfb5dfc3c7574229cd2
Jakob Metzler <jakob.metzler@gmx.net>
2024-01-15 21:32:32 +0100

    Fixing inconsistent use of tabs in particles.py

--- code/particles.py
+++ code/particles.py
@@ -42,15 +42,14 @@ class AnimationPlayer:
 	
 	def reflect_images(self,frames):
 		new_frames = []
-
 		for frame in frames:
-	 		flipped_frame = pygame.transform.flip(frame,True,False)
-	 		new_frames.append(flipped_frame)
+			flipped_frame = pygame.transform.flip(frame,True,False)
+			new_frames.append(flipped_frame)
 		return new_frames
-
+	
 	def create_grass_particles(self,pos,groups):
-	 	animation_frames = choice(self.frames['leaf'])
-	 	ParticleEffect(pos,animation_frames,groups)
+		animation_frames = choice(self.frames['leaf'])
+		ParticleEffect(pos,animation_frames,groups)
 
 	def create_particles(self,animation_type,pos,groups):
 		animation_frames = self.frames[animation_type]
